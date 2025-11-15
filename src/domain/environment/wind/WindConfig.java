9eff5ae6d52169f3a8ea7687919b967e70af4c07
choiyoung69 <choiyoung609@gmail.com>
2025-11-15 17:19:45 +0900

    feat(WindConfig): 바람의 세기를 결정할 config 생성

---
+++ src/domain/environment/wind/WindConfig.java
@@ -0,0 +1,11 @@
+package domain.environment.wind;
+
+public record WindConfig (
+    double speedVariability, //풍속 변화량, 게임 레벨에 따라 변동성이 커짐
+    double directionVariability, //
+
+    double gustChance,  //돌풍 발생 확률
+    double lullChance,  //약풍 발생 확률
+    double shiftChance, //풍향 급변 확률
+    double turbulenceChance, //난류 발생 확률
+) { }
