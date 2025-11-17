8ae5bda5830718b7609f62568cfc73e9e281dd1d
choiyoung69 <choiyoung609@gmail.com>
2025-11-18 03:31:21 +0900

    feat(EventOptions): 이벤트 옵션을 관리하는 일급 컬렉션 생성

--- src/domain/event/Event.java
+++ src/domain/event/Event.java
@@ -1,15 +1,23 @@
 package domain.event;
 
+import java.util.List;
+
 public class Event {
     private final EventType eventType;
     private final String message;
+    private final EventOptions eventOptions;
 
-    private Event(EventType eventType, String message) {
+    public Event(EventType eventType, String message, EventOptions eventOptions) {
         this.eventType = eventType;
         this.message = message;
+        this.eventOptions = eventOptions;
     }
 
-    public static Event create(EventType eventType, String message) {
-        return new Event(eventType, message);
+    public static Event create(EventType eventType, String message, EventOptions eventOptions) {
+        return new Event(eventType, message, eventOptions);
+    }
+
+    public void resolve(EventOption selected) {
+        selected.effect();
     }
 }
---
+++ src/domain/event/EventOptions.java
@@ -0,0 +1,12 @@
+package domain.event;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class EventOptions {
+    private final List<EventOption> eventOptions;
+
+    public EventOptions(List<EventOption> eventOptions) {
+        this.eventOptions = eventOptions;
+    }
+}
