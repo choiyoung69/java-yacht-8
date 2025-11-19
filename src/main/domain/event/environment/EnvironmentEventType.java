8a30a9aa06a99648c654683a9d179bd4248f29e9
choiyoung69 <choiyoung609@gmail.com>
2025-11-20 02:51:19 +0900

    feat(EnvironmentEventType): environment 이벤트를 세세하게 나눠서 관리

---
+++ src/main/domain/event/environment/EnvironmentEventType.java
@@ -0,0 +1,35 @@
+package main.domain.event.environment;
+
+public enum EnvironmentEventType {
+    // Wind 자연 풍속 이벤트
+    WIND_WEAK_TO_NORMAL(EventCategory.NATURAL, EventSource.WIND),
+    WIND_NORMAL_TO_STRONG(EventCategory.NATURAL, EventSource.WIND),
+    WIND_STRONG_TO_NORMAL(EventCategory.NATURAL, EventSource.WIND),
+    WIND_NORMAL_TO_WEAK(EventCategory.NATURAL, EventSource.WIND),
+
+    // Wind 자연 풍향 이벤트
+    WIND_DIRECTION_UNDER_THRESHOLD(EventCategory.NATURAL, EventSource.WIND),
+    WIND_DIRECTION_OVER_THRESHOLD(EventCategory.NATURAL, EventSource.WIND),
+
+    // Wind 랜덤 이벤트
+    WIND_GUST(EventCategory.RANDOM, EventSource.WIND),
+    WIND_LULL(EventCategory.RANDOM, EventSource.WIND),
+    WIND_TURBULENCE(EventCategory.RANDOM, EventSource.WIND),
+    WIND_RANDOM_SHIFT(EventCategory.RANDOM, EventSource.WIND);
+
+    private final EventCategory category;
+    private final EventSource source;
+
+    EnvironmentEventType(EventCategory category, EventSource source) {
+        this.category = category;
+        this.source = source;
+    }
+
+    public EventCategory category() {
+        return category;
+    }
+
+    public EventSource source() {
+        return source;
+    }
+}
